7859075704d2da4012967883f7457c7cabbfcffc
Alessandro1979-itac <sandromuniz27@hotmail.com>
2021-09-21 21:30:01 -0400

    CRUD - médicos hospitales e usuários

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,3 @@
-/*
-    Ruta: /api/usuarios
-*/
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
