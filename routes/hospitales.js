dc59ef9bcd52d23706e71c743801266296e5c3b1
Alessandro1979-itac <sandromuniz27@hotmail.com>
2021-09-29 14:11:04 -0400

    Mecanismo de busca, serviços de menu e AdminGuard

--- routes/hospitales.js
+++ routes/hospitales.js
@@ -1,48 +1,34 @@
-/*
-    Hospitales
-    ruta: '/api/hospitales'
-*/
-const { Router } = require('express');
-const { check } = require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
-
-const { validarJWT } = require('../middlewares/validar-jwt');
-
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 const {
-    getHospitales,
-    crearHospital,
-    actualizarHospital,
-    borrarHospital
-} = require('../controllers/hospitales')
-
-
+  getHospitales,
+  crearHospital,
+  actualizarHospital,
+  borrarHospital,
+} = require("../controllers/hospitales");
 const router = Router();
 
-router.get( '/', getHospitales );
-
-router.post( '/',
-    [
-        validarJWT,
-        check('nombre','El nombre del hospital es necesario').not().isEmpty(),
-        validarCampos
-    ], 
-    crearHospital 
-);
-
-router.put( '/:id',
-    [
-        validarJWT,
-        check('nombre','El nombre del hospital es necesario').not().isEmpty(),
-        validarCampos
-    ],
-    actualizarHospital
-);
-
-router.delete( '/:id',
+router.get("/", getHospitales);
+router.post(
+  "/",
+  [
     validarJWT,
-    borrarHospital
+    check("nombre", "El nombre del hospital es necesario").not().isEmpty(),
+    validarCampos,
+  ],
+  crearHospital
 );
-
-
+router.put(
+  "/:id",
+  [
+    validarJWT,
+    check("nombre", "El nombre del hospital es necesario").not().isEmpty(),
+    validarCampos,
+  ],
+  actualizarHospital
+);
+router.delete("/:id", validarJWT, borrarHospital);
 
 module.exports = router;
--- routes/medicos.js
+++ routes/medicos.js
@@ -1,59 +1,38 @@
-/*
-    Medicos
-    ruta: '/api/medico'
-*/
-const { Router } = require('express');
-const { check } = require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
-
-const { validarJWT } = require('../middlewares/validar-jwt');
-
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 const {
-    getMedicos,
-    crearMedico,
-    actualizarMedico,
-    borrarMedico,
-    getMedicoById
-} = require('../controllers/medicos')
-
-
+  getMedicos,
+  crearMedico,
+  actualizarMedico,
+  borrarMedico,
+  getMedicoById,
+} = require("../controllers/medicos");
 const router = Router();
 
-router.get( '/', validarJWT, getMedicos );
-
-router.post( '/',
-    [
-        validarJWT,
-        check('nombre','El nombre del médico es necesario').not().isEmpty(),
-        check('hospital','El hospital id debe de ser válido').isMongoId(),
-        validarCampos
-    ], 
-    crearMedico 
-);
-
-router.put( '/:id',
-    [
-        validarJWT,
-        check('nombre','El nombre del médico es necesario').not().isEmpty(),
-        check('hospital','El hospital id debe de ser válido').isMongoId(),
-        validarCampos
-    ],
-    actualizarMedico
-);
-
-router.delete( '/:id',
+router.get("/", validarJWT, getMedicos);
+router.post(
+  "/",
+  [
     validarJWT,
-    borrarMedico
+    check("nombre", "El nombre del médico es necesario").not().isEmpty(),
+    check("hospital", "El hospital id debe de ser válido").isMongoId(),
+    validarCampos,
+  ],
+  crearMedico
 );
-
-router.get( '/:id',
+router.put(
+  "/:id",
+  [
     validarJWT,
-    getMedicoById
+    check("nombre", "El nombre del médico es necesario").not().isEmpty(),
+    check("hospital", "El hospital id debe de ser válido").isMongoId(),
+    validarCampos,
+  ],
+  actualizarMedico
 );
-
-
+router.delete("/:id", validarJWT, borrarMedico);
+router.get("/:id", validarJWT, getMedicoById);
 
 module.exports = router;
-
-
-
